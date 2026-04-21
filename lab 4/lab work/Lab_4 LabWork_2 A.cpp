51f059aa8e6445deea2e3af74239cb8be776a588
fazilsafdar56-jpg <fazilsafdar56@gmail.com>
2026-04-21 12:54:51 +0500

    Update Lab_4 LabWork_2 A.cpp

--- lab 4/lab work/Lab_4 LabWork_2 A.cpp
+++ lab 4/lab work/Lab_4 LabWork_2 A.cpp
@@ -1,46 +1,49 @@
-// Name:muhammad fazil safdar
+// Name: Muhammad Fazil Safdar
 // Department: Electrical Engineering (Power)
 // Registration No: BF25NWELE0724
 // Section: A
 
-#include <iostream>
-using namespace std;
+#include <iostream>   // Library for input and output
+using namespace std;  // To use standard names like cout and cin
 
 int main() {
-    char grade;
+    char grade;   // Variable to store grade (character)
 
+    // Ask user to enter grade
     cout << "Enter your grade (A, B, C, D, F): ";
     cin >> grade;
 
+    // Switch statement to check grade
     switch(grade) {
         case 'A':
         case 'a':
-            cout << "Excellent";
+            cout << "Excellent" << endl;   // For grade A or a
             break;
 
         case 'B':
         case 'b':
-            cout << "Very Good";
+            cout << "Very Good" << endl;   // For grade B or b
             break;
 
         case 'C':
         case 'c':
-            cout << "Good";
+            cout << "Good" << endl;   // For grade C or c
             break;
 
         case 'D':
         case 'd':
-            cout << "Pass";
+            cout << "Pass" << endl;   // For grade D or d
             break;
 
         case 'F':
         case 'f':
-            cout << "Fail";
+            cout << "Fail" << endl;   // For grade F or f
             break;
 
         default:
-            cout << "Invalid Grade";
+            // Runs if input is not a valid grade
+            cout << "Invalid Grade" << endl;
     }
 
-    return 0;
+    return 0;   // End of program
 }
